6519000853498c409cbb73f8281d58625a9b92cd
dominic montelongo and kaya jepson <kaya@>
2019-04-01 14:48:47 -0700

    take input, capitalize the words, and return them as a list

--- js/scripts.js
+++ js/scripts.js
@@ -7,13 +7,37 @@ $(document).ready(function() {
     var array1 = [things1Input, things2Input, things3Input];
 
     $("#answerArea").show();
-    $("#display").text(array1);
+    // $("#quiz").hide();
+    // $("#display").text(array1);
+    // $("#display").text(array1);
     event.preventDefault();
 
     var array2 = [];
-    array2.push(array1[0], array1[1], array1[2]);
-    $("#elementZero").text("We also love " + array2[0]);
-    $("#elementOne").text("We also love " + array2[1]);
-    $("#elementTwo").text("We also love " + array2[2]);
+    var array3 = [];
+    var array4 = [];
+    // array2.push(array1[0], array1[1], array1[2]);
+    // $("#elementZero").text("We also love " + array2[0]);
+    // $("#elementOne").text("We also love " + array2[1]);
+    // $("#elementTwo").text("We also love " + array2[2]);
+    array2.push(array1[0]);
+    array2.push(array1[1]);
+    array2.push(array1[2]);
+    $("#elementZeroTwo").text("We also love " + array2[0]);
+    $("#elementOneTwo").text("We also love " + array2[1]);
+    $("#elementTwoTwo").text("We also love " + array2[2]);
+
+    var array3 = array2.map(function(upper) {
+        return (upper.toUpperCase());
+        // return $("#display" + (upper.toUpperCase()));
+    });
+    $("#display").text(array3);
+    console.log(array3);
+
+    array3.push(array1[0]);
+    array3.push(array1[1]);
+    array3.push(array1[2]);
+    $("#elementZeroTwo").text("We also love " + array3[0]);
+    $("#elementOneTwo").text("We also love " + array3[1]);
+    $("#elementTwoTwo").text("We also love " + array3[2]);
   });
 });
